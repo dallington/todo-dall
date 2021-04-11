18c5e7395ae35777cdf96ce9354bf922fc9b65df
Dallington Augusto <contato@dallington.com.br>
2021-04-10 21:29:22 -0300

    Remove function in utils function

--- src/Utils/functions.tsx
+++ src/Utils/functions.tsx
@@ -1,4 +1,4 @@
-export function isNumber(x: any): boolean {
+export function isNumber(x: any): as string {
     return typeof x === "number";
 }
 export function isString(x: any): boolean {
@@ -10,7 +10,3 @@ export function isObject(x: any): boolean {
 export function isFunction(x: any): boolean {
     return typeof x === "function";
 }
-
-export function getKeysFromInterface<T extends object>(): Array<keyof T> {
-    return []
-};
